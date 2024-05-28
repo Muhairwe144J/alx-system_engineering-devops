32404200c3ac77354d163ea2683728089d068795
Muhairwe144J <mnasasirashafik144j@gmail.com>
2024-05-28 22:23:33 +0300

    ssh

---
+++ 0x0B-ssh/add_ssh_key.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Ensure the script is run as root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+# Switch to ubuntu user and create .ssh directory if it doesn't exist
+sudo -u ubuntu bash << EOF
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
+
+# Add the provided SSH public key to the authorized_keys file
+echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDNdtrNGtTXe5Tp1EJQop8mOSAuRGLjJ6DW4PqX4wId/Kawz35ESampIqHSOTJmbQ8UlxdJuk0gAXKk3Ncle4safGYqM/VeDK3LN5iAJxf4kcaxNtS3eVxWBE5iF3FbIjOqwxw5Lf5sRa5yXxA8HfWidhbIG5TqKL922hPgsCGABIrXRlfZYeC0FEuPWdr6smOElSVvIXthRWp9cr685KdCI+COxlj1RdVsvIo+zunmLACF9PYdjB2s96Fn0ocD3c5SGLvDOFCyvDojSAOyE70ebIElnskKsDTGwfT4P6jh9OBzTyQEIS2jOaE5RQq4IB4DsMhvbjDSQrP0MdCLgwkN' >> ~/.ssh/authorized_keys
+
+# Set the correct permissions
+chmod 600 ~/.ssh/authorized_keys
+EOF
+
+echo "SSH key added successfully for the ubuntu user."
